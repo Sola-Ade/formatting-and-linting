7df0c1493e823a8ca7b89fbad463099e6694240f
Sola-Ade <solaadefarasin@hotmail.com>
2024-06-10 16:36:02 +0100

    More changes.

--- src/count-occurrences.js
+++ src/count-occurrences.js
@@ -15,7 +15,7 @@ function x(y,n){
         for(let mars of y){
                 if(n=mars){x2++}}
                 return x2
-        }
+        
 
 console.log(x(["na", "na", "na", "na", "BATMAN"], "na"), 4);
 console.log(x(["na", "na", "na", "na", "BATMAN"], "BATMAN"), 1);
